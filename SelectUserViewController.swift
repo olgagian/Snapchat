b424a1a7202b077ea43ada2fb4320c18e78721f1
olgagian <ioannis@poleiseis.giannakidis.local>
2017-02-15 18:27:35 +0200

    selectUserTablevieW_insert

--- SelectUserViewController.swift
+++ SelectUserViewController.swift
@@ -8,28 +8,24 @@
 
 import UIKit
 
-class SelectUserViewController: UIViewController {
+class SelectUserViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        
+    
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    }
+    
 }
